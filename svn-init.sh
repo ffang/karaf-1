22a428ca50e750d496e68bb67c26d83c660c1587
Gert Vanthienen <gert.vanthienen@gmail.com>
2010-09-24 02:08:50 +0200

    Using git-mirror to get Apache repo mirrored into the FuseSource one

--- svn-init.sh
+++ svn-init.sh
@@ -1,4 +1,8 @@
 PROJECT_NAME=karaf
+REPOSITORY="git://git.apache.org/karaf.git"
+REMOTE=apache
+
+git mirror $REPOSITORY --prefix=$REMOTE
 
 echo "Updating .git/authors.txt"
 cd ".git"
@@ -6,6 +10,4 @@ curl "http://git.apache.org/authors.txt" > authors.txt
 cd ".."
 git config svn.authorsfile ".git/authors.txt"
 echo "svn init"
-git svn init --prefix=origin/ --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/"$PROJECT_NAME"
-echo "svn fetch"
-git svn fetch
+git svn init --prefix="$REMOTE/" --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/"$PROJECT_NAME"
