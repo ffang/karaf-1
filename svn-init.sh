ca0e46924346984396dbcd8d977c1a0bb8d68e9a
Gert Vanthienen <gert.vanthienen@gmail.com>
2010-09-24 16:30:21 +0200

    Using a remote repository for apache instead of git mirror

--- svn-init.sh
+++ svn-init.sh
@@ -3,7 +3,8 @@ REPOSITORY="git://git.apache.org/karaf.git"
 REMOTE=apache
 
 echo "Configuring $REPOSITORY as remote $REMOTE"
-git mirror $REPOSITORY --prefix=$REMOTE
+git remote add $REMOTE $REPOSITORY
+git fetch $REPOSITORY
 
 echo "Updating .git/authors.txt"
 cd ".git"
@@ -13,7 +14,3 @@ git config svn.authorsfile ".git/authors.txt"
 
 echo "svn init"
 git svn init --prefix="$REMOTE/" --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/"$PROJECT_NAME"
-
-echo "svn rebase"
-git checkout "apache-trunk"
-git svn rebase
