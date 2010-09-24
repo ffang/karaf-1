64494aa428ef9b9f9251baf8dab8630cd32ec498
Gert Vanthienen <gert.vanthienen@gmail.com>
2010-09-24 16:52:53 +0200

    Fix git fetch and add initial rebase to rebuild git-svn metadata

--- svn-init.sh
+++ svn-init.sh
@@ -4,7 +4,7 @@ REMOTE=apache
 
 echo "Configuring $REPOSITORY as remote $REMOTE"
 git remote add $REMOTE $REPOSITORY
-git fetch $REPOSITORY
+git fetch $REMOTE
 
 echo "Updating .git/authors.txt"
 cd ".git"
@@ -14,3 +14,10 @@ git config svn.authorsfile ".git/authors.txt"
 
 echo "svn init"
 git svn init --prefix="$REMOTE/" --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/"$PROJECT_NAME"
+
+echo "svn rebase"
+git checkout trunk
+git svn rebase
+
+echo "Done initializing git repository"
+git checkout master
