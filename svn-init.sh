f622615de9b459e7ce6e1875e73e2d4e27841e7f
Gert Vanthienen <gert.vanthienen@gmail.com>
2010-09-24 10:15:50 +0200

    A few aesthetic changes to the init script

--- svn-init.sh
+++ svn-init.sh
@@ -2,6 +2,7 @@ PROJECT_NAME=karaf
 REPOSITORY="git://git.apache.org/karaf.git"
 REMOTE=apache
 
+echo "Configuring $REPOSITORY as remote $REMOTE"
 git mirror $REPOSITORY --prefix=$REMOTE
 
 echo "Updating .git/authors.txt"
@@ -9,5 +10,10 @@ cd ".git"
 curl "http://git.apache.org/authors.txt" > authors.txt
 cd ".."
 git config svn.authorsfile ".git/authors.txt"
+
 echo "svn init"
 git svn init --prefix="$REMOTE/" --tags=tags --trunk=trunk --branches=branches https://svn.apache.org/repos/asf/"$PROJECT_NAME"
+
+echo "svn rebase"
+git checkout "apache-trunk"
+git svn rebase
