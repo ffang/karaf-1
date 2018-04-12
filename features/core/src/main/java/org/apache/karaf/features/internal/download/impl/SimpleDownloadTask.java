df79ac36b3cd06bcfcc07db7fcea20bacc514fed
Grzegorz Grzybek <ggrzybek@redhat.com>
2018-04-12 13:24:08 +0200

    Revert "[KARAF-5694]strip url to ensure it's a valid one which could download"
    This reverts commit 2e9735d747a755c645cb2484a01c840a6a481f55.

--- features/core/src/main/java/org/apache/karaf/features/internal/download/impl/SimpleDownloadTask.java
+++ features/core/src/main/java/org/apache/karaf/features/internal/download/impl/SimpleDownloadTask.java
@@ -72,8 +72,7 @@ public class SimpleDownloadTask extends AbstractRetryableDownloadTask {
             }
 
             File tmpFile = File.createTempFile("download-", null, dir);
-            
-            urlObj = new URL(DownloadManagerHelper.stripUrl(urlObj.toString()));
+
             try (InputStream is = urlObj.openStream();
                  OutputStream os = new FileOutputStream(tmpFile)) {
                 StreamUtils.copy(is, os);
