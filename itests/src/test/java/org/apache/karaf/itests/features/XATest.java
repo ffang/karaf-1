74c971bfbb1815b036dd3fa5c1dc0481d014aea2
Jean-Baptiste Onofré <jbonofre@apache.org>
2018-05-04 12:47:45 +0200

    [KARAF-5721] Upgrade to PAX JMS 1.0.1 [KARAF-5723] Upgrade to PAX JDBC 1.3.0 [KARAF-5771] Upgrade to Pax Transx 0.3.0
    (cherry picked from commit 68f7c5c74b9306df85ae1d3a3497525c4bfd7354)
    
    Conflicts:
    	itests/src/test/java/org/apache/karaf/itests/features/XATest.java
    	pom.xml

--- itests/src/test/java/org/apache/karaf/itests/features/XATest.java
+++ itests/src/test/java/org/apache/karaf/itests/features/XATest.java
@@ -16,6 +16,7 @@ package org.apache.karaf.itests.features;
 import org.apache.karaf.features.FeaturesService;
 import org.apache.karaf.itests.KarafTestSupport;
 import org.apache.karaf.itests.util.RunIfRule;
+import org.apache.karaf.jaas.boot.principal.RolePrincipal;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -25,6 +26,7 @@ import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.PaxExam;
 import org.ops4j.pax.exam.spi.reactors.ExamReactorStrategy;
 import org.ops4j.pax.exam.spi.reactors.PerClass;
+import org.ops4j.pax.exam.spi.reactors.PerMethod;
 import org.osgi.framework.Bundle;
 
 import java.util.Arrays;
@@ -55,8 +57,8 @@ public class XATest extends KarafTestSupport {
                 "mvn:org.apache.karaf.features/enterprise/" + version + "/xml/features, " +
                 "mvn:org.apache.karaf.features/spring-legacy/" + version + "/xml/features, " +
                 "mvn:org.apache.karaf.features/standard/" + version + "/xml/features, " +
-                "mvn:org.apache.activemq/artemis-features/2.2.0/xml/features, " +
-                "mvn:org.apache.camel.karaf/apache-camel/2.19.2/xml/features"
+                "mvn:org.apache.activemq/artemis-features/2.6.0/xml/features, " +
+                "mvn:org.apache.camel.karaf/apache-camel/2.20.1/xml/features"
             ));
         result.add(replaceConfigurationFile("etc/org.ops4j.connectionfactory-artemis.cfg", getConfigFile("/org/apache/karaf/itests/features/org.ops4j.connectionfactory-artemis.cfg")));
         result.add(replaceConfigurationFile("etc/org.ops4j.datasource-derby.cfg", getConfigFile("/org/apache/karaf/itests/features/org.ops4j.datasource-derby.cfg")));
@@ -71,39 +73,53 @@ public class XATest extends KarafTestSupport {
     }
 
     @Test
-    public void installFeature() throws Exception {
-        featureService.installFeatures(asSet(
-                "transaction",
-                "transaction-manager-narayana",
-                "artemis",
-                "pax-jms-pool",
-                "jms",
-                "pax-jdbc-derby",
-                "pax-jdbc-pool-transx",
-                "jdbc",
-                "shell-compat",
-                "camel-blueprint",
-                "camel-spring",
-                "camel-sql",
-                "camel-jms"), NO_AUTO_REFRESH);
+    public void test() throws Exception {
+        featureService.installFeature("aries-blueprint");
+
+        System.out.println("== Installing Artemis");
+        featureService.installFeature("artemis", NO_AUTO_REFRESH);
+        Thread.sleep(15000);//sleep a while ensure the jms broker is up
+        featureService.installFeature("jms", NO_AUTO_REFRESH);
+        featureService.installFeature("pax-jms-artemis", NO_AUTO_REFRESH);
+
+        System.out.println(" ");
+        System.out.println(executeCommand("jms:info artemis", new RolePrincipal("admin")));
+
+        System.out.println("== Installing Derby");
+        featureService.installFeature("jdbc", NO_AUTO_REFRESH);
+        featureService.installFeature("pax-jdbc-derby", NO_AUTO_REFRESH);
+        featureService.installFeature("pax-jdbc-pool-transx", NO_AUTO_REFRESH);
+
+        System.out.println(" ");
+        System.out.println(executeCommand("jdbc:ds-list", new RolePrincipal("admin")));
+
+        System.out.println("== Install Camel route");
+        featureService.installFeature("camel-blueprint", NO_AUTO_REFRESH);
+        featureService.installFeature("camel-sql", NO_AUTO_REFRESH);
+        featureService.installFeature("camel-jms", NO_AUTO_REFRESH);
+
+        featureService.installFeature("transaction-manager-narayana", NO_AUTO_REFRESH);
 
         Bundle bundle = bundleContext.installBundle("blueprint:file:etc/xa-test-camel.xml");
         bundle.start();
 
-        executeCommand("jdbc:execute derby CREATE TABLE messages (id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, message VARCHAR(1024) NOT NULL, CONSTRAINT primary_key PRIMARY KEY (id))", new org.apache.karaf.jaas.boot.principal.RolePrincipal("admin"));
-        executeCommand("jms:send artemis MyQueue 'the-message'", new org.apache.karaf.jaas.boot.principal.RolePrincipal("admin"));
+        Thread.sleep(5000);
 
-        Thread.sleep(1000);
+        System.out.println(executeCommand("camel:route-list", new RolePrincipal("admin")));
+
+        System.out.println("== Creating tables in Derby");
+        System.out.println(executeCommand("jdbc:execute derby CREATE TABLE messages (id INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY, message VARCHAR(1024) NOT NULL, CONSTRAINT primary_key PRIMARY KEY (id))", new RolePrincipal("admin")));
+
+        System.out.println("== Sending a message in Artemis broker that should be consumed by Camel route and inserted into the Derby database");
+        System.out.println(executeCommand("jms:send artemis MyQueue 'the-message'", new RolePrincipal("admin")));
+
+        Thread.sleep(5000);
 
         String output = executeCommand("jdbc:query derby select * from messages", new org.apache.karaf.jaas.boot.principal.RolePrincipal("admin"));
         System.err.println(output);
 
         assertContains("the-message", output);
 
-    }
-
-    private static Set<String> asSet(String... strings) {
-        return new HashSet<>(Arrays.asList(strings));
     }
 
 }
